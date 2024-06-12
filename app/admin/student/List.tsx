029ff5bb1d432ceb1ee1fa82e4125dda137e6d75
KingBonfante <72134861+KingBonfante@users.noreply.github.com>
2024-06-12 01:09:49 +0000

    feat: delete

--- app/admin/student/List.tsx
+++ app/admin/student/List.tsx
@@ -1,50 +1,60 @@
+import { Button } from "@/components/ui/button"
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableFooter,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table"
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 import { revalidatePath } from "next/cache"
 
-  interface IStudent{
-    id:number,
-    name:string,
-    email:string
-  }
-  
-  export default async function ListStudent() {
-    const students = await list()
-    async function list(){
-      revalidatePath("/admin/student")
-      const response = await fetch("https://server20241-six.vercel.app/students")
-        return response.json();
-    }
+interface IStudent {
+  id: number,
+  name: string,
+  email: string
+}
 
-    return (
-      <Table>
-        <TableCaption>Lista de Estudantes</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">ID</TableHead>
-            <TableHead>Nome</TableHead>
-            <TableHead>E-mail</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {students.map((item:IStudent) => (
-            <TableRow key={item.id}>
-              <TableCell className="font-medium">{item.id}</TableCell>
-              <TableCell>{item.name}</TableCell>
-              <TableCell>{item.email}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-       
-      </Table>
-    )
+export default async function ListStudent() {
+  const students = await list()
+  async function list() {
+    revalidatePath("/admin/student")
+    const response = await fetch("https://server20241-six.vercel.app/students")
+    return response.json();
   }
+
+
   
+
+  return (
+    <Table>
+      <TableCaption>Lista de Estudantes</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">ID</TableHead>
+          <TableHead>Nome</TableHead>
+          <TableHead>E-mail</TableHead>
+          <TableHead>Ação</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {students.map((item: IStudent) => (
+          <TableRow key={item.id}>
+            <TableCell className="font-medium">{item.id}</TableCell>
+            <TableCell>{item.name}</TableCell>
+            <TableCell>{item.email}</TableCell>
+            <TableCell>
+              <form>
+                <input type="text" name="id" hidden value={item.id} />
+                <Button formAction={deleteStudent} variant="destructive">Excluir</Button>
+              </form>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+
+    </Table >
+  )
+}
