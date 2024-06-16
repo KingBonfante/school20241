0110f039de5399e33ba1d8fd4644a583b2ead252
KingBonfante <72134861+KingBonfante@users.noreply.github.com>
2024-06-16 22:26:55 +0000

    fix: Correção tela admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -7,9 +7,6 @@ export default function Admin() {
     return (
         <div>
             <h1 className="text-center">Bem Vindo à Área Administrativa</h1>
-            <Button>Salvar</Button>
-            <Button variant="destructive">Excluir</Button>
-            <Button variant="secondary" >Novo</Button>
 
             <MenuSheet/>
            
